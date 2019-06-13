9848144f99f5ae2bd595f9bac1463f766eafaae0
Nelson Gill <robotrogue@hotmail.com>
2019-06-13 10:51:06 -0700

    Add css and js stubs

---
+++ content.js
@@ -0,0 +1,33 @@
+javascript: (function() {
+  var playerContainer = document.querySelector("div.l-col-fluid");
+  if (playerContainer) {
+    playerContainer.setAttribute("style", "width: 1050px; height: 610px;");
+  }
+  var textBlurb = document.querySelector("aside.l-col-fixed");
+  if (textBlurb) {
+    textBlurb.className = "l-col-fluid";
+  }
+  var background = document.querySelector("#panel");
+  if (background) {
+    background.setAttribute("style", "background: #101010 !important;");
+  }
+  var upperBackground = document.querySelector("section.upper");
+  if (upperBackground) {
+    upperBackground.setAttribute("style", "background: #101010 !important;");
+  }
+  var containerBackground = document.querySelector(".featured-publishable");
+  if (containerBackground) {
+    containerBackground.setAttribute(
+      "style",
+      "background: #424242 !important;"
+    );
+  }
+  var containerDescriptionText = document.querySelector(".description");
+  if (containerDescriptionText) {
+    containerDescriptionText.setAttribute("style", "color: white;");
+  }
+  var carouselBack = document.querySelector(".promo-carousel-revamp");
+  if (carouselBack) {
+    carouselBack.setAttribute("style", "background: #424242 !important;");
+  }
+})();
