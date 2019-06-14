46e5f582d4d1f979c318ba1326b2f84776454388
Nelson Gill <robotrogue@hotmail.com>
2019-06-14 14:08:24 -0700

    Syntax cleanup

--- content.js
+++ content.js
@@ -1,3 +1,45 @@
+javascript: (function() {
+  var hasFeatureVideo = document.querySelector(".featured-publishable-video");
+  document.querySelector("#id_term").setAttribute("style", "width: 252px;");
+  document
+    .querySelector(".header-sitenav .subscribe a")
+    .setAttribute("style", "color: #00ccff;");
+  if (hasFeatureVideo) {
+    document
+      .querySelector("div.l-col-fluid")
+      .setAttribute("style", "width: 1050px; height: 610px;");
+    document.querySelector("aside.l-col-fixed").className = "l-col-fluid";
+    document
+      .querySelector("#panel")
+      .setAttribute(
+        "style",
+        "background: url(https://d2rormqr1qwzpz.cloudfront.net/static/site/img/layout/bg-grid-alt.png) #101010 !important;"
+      );
+    document
+      .querySelector("section.upper")
+      .setAttribute("style", "background: #101010 !important;");
+    document
+      .querySelector(".featured-publishable")
+      .setAttribute("style", "background: #424242 !important;");
+    document
+      .querySelector(".description")
+      .setAttribute("style", "color: white;");
+    document
+      .querySelector(".promo-carousel-revamp")
+      .setAttribute("style", "background: #424242 !important;");
+    document
+      .querySelector(".comments-wrap")
+      .setAttribute("style", "background: #424242 !important;");
+    document.querySelector("a.title").setAttribute("style", "color: #cacaca;");
+    document
+      .querySelector("span.author")
+      .setAttribute("style", "color: #e05e00;");
+    document
+      .querySelector("a.comment-count")
+      .setAttribute("style", "color: #e05e00;");
+  }
+})();
+
 // javascript: (function() {
 //   var searchBar = document.querySelector("#id_term");
 //   if (searchBar) {
@@ -48,35 +90,3 @@
 //     .querySelector("a.comment-count")
 //     .setAttribute("style", "color: #e05e00;");
 // })();
-
-javascript: (function() {
-  var featureVideo = document.querySelector('.featured-publishable-video')
-  document.querySelector("#id_term").setAttribute("style", "width: 252px;");
-  if (featureVideo) {
-  document
-    .querySelector("div.l-col-fluid")
-    .setAttribute("style", "width: 1050px; height: 610px;");
-  document.querySelector("aside.l-col-fixed").className = "l-col-fluid";
-  document
-    .querySelector("#panel")
-    .setAttribute(
-      "style",
-      "background: url(https://d2rormqr1qwzpz.cloudfront.net/static/site/img/layout/bg-grid-alt.png) #101010 !important;"
-    );
-  document
-    .querySelector("section.upper")
-    .setAttribute("style", "background: #101010 !important;");
-  document
-    .querySelector(".featured-publishable")
-    .setAttribute("style", "background: #424242 !important;");
-  document.querySelector(".description").setAttribute("style", "color: white;");
-  document
-    .querySelector(".promo-carousel-revamp")
-    .setAttribute("style", "background: #424242 !important;");
-  document.querySelector("a.title").setAttribute("style", "color: #cacaca;");
-  document.querySelector("span.author").setAttribute("style", "color: #e05e00;");
-  document
-    .querySelector("a.comment-count")
-    .setAttribute("style", "color: #e05e00;");
-  }
-})();
