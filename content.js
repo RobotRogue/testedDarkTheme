d8bfdfc1ae8cbb8ec9f3fe785af71064e3490197
Nelson Gill <robotrogue@hotmail.com>
2019-06-14 16:23:39 -0700

    Fix code errors - added contidionals

--- content.js
+++ content.js
@@ -1,92 +1,70 @@
 javascript: (function() {
-  var hasFeatureVideo = document.querySelector(".featured-publishable-video");
   document.querySelector("#id_term").setAttribute("style", "width: 252px;");
   document
     .querySelector(".header-sitenav .subscribe a")
     .setAttribute("style", "color: #00ccff;");
+
+  var hasFeatureVideo = document.querySelector(".featured-publishable-video");
   if (hasFeatureVideo) {
-    document
-      .querySelector("div.l-col-fluid")
-      .setAttribute("style", "width: 1050px; height: 610px;");
-    document.querySelector("aside.l-col-fixed").className = "l-col-fluid";
-    document
-      .querySelector("#panel")
-      .setAttribute(
+    var vidPlayer = document.querySelector("div.l-col-fluid");
+    if (vidPlayer) {
+      vidPlayer.setAttribute("style", "width: 1050px; height: 610px;");
+    }
+
+    var videoBlurb = document.querySelector("aside.l-col-fixed");
+    if (videoBlurb) {
+      videoBlurb.className = "l-col-fluid";
+    }
+
+    var pageBackground = document.querySelector("#panel");
+    if (pageBackground) {
+      pageBackground.setAttribute(
         "style",
         "background: url(https://d2rormqr1qwzpz.cloudfront.net/static/site/img/layout/bg-grid-alt.png) #101010 !important;"
       );
-    document
-      .querySelector("section.upper")
-      .setAttribute("style", "background: #101010 !important;");
-    document
-      .querySelector(".featured-publishable")
-      .setAttribute("style", "background: #424242 !important;");
-    document
-      .querySelector(".description")
-      .setAttribute("style", "color: white;");
-    document
-      .querySelector(".promo-carousel-revamp")
-      .setAttribute("style", "background: #424242 !important;");
-    document
-      .querySelector(".comments-wrap")
-      .setAttribute("style", "background: #424242 !important;");
-    document.querySelector("a.title").setAttribute("style", "color: #cacaca;");
-    document
-      .querySelector("span.author")
-      .setAttribute("style", "color: #e05e00;");
-    document
-      .querySelector("a.comment-count")
-      .setAttribute("style", "color: #e05e00;");
+    }
+
+    var titleBar = document.querySelector("section.upper");
+    if (titleBar) {
+      titleBar.setAttribute("style", "background: #000000 !important;");
+    }
+
+    var articleContainer = document.querySelector(".featured-publishable");
+    if (articleContainer) {
+      articleContainer.setAttribute("style", "background: #424242 !important;");
+    }
+
+    var descriptionText = document.querySelector(".description");
+    if (descriptionText) {
+      descriptionText.setAttribute("style", "color: white;");
+    }
+
+    var promoCarousel = document.querySelector(".promo-carousel-revamp");
+    if (promoCarousel) {
+      promoCarousel.setAttribute("style", "background: #424242 !important;");
+    }
+
+    var commentsContainer = document.querySelector(".comments-wrap");
+    if (commentsContainer) {
+      commentsContainer.setAttribute(
+        "style",
+        "background: #424242 !important;"
+      );
+    }
+
+    var descriptionTitle = document.querySelector("a.title");
+    if (descriptionTitle) {
+      descriptionTitle.setAttribute("style", "color: #cacaca;");
+    }
+
+    var author = document.querySelector("span.author");
+    if (author) {
+      author.setAttribute("style", "color: #e05e00;");
+    }
+
+    var commentCounter = document.querySelector("a.comment-count");
+    if (commentCounter) {
+      commentCounter.setAttribute("style", "color: #e05e00;");
+    }
   }
 })();
-
-// javascript: (function() {
-//   var searchBar = document.querySelector("#id_term");
-//   if (searchBar) {
-//     searchBar.setAttribute("style", "width: 252px;");
-//   }
-//   var playerContainer = document.querySelector("div.l-col-fluid");
-//   if (playerContainer) {
-//     playerContainer.setAttribute("style", "width: 1050px; height: 610px;");
-//   }
-//   var textBlurb = document.querySelector("aside.l-col-fixed");
-//   if (textBlurb) {
-//     textBlurb.className = "l-col-fluid";
-//   }
-//   var background = document.querySelector("#panel");
-//   if (background) {
-//     background.setAttribute(
-//       "style",
-//       "background: url(https://d2rormqr1qwzpz.cloudfront.net/static/site/img/layout/bg-grid-alt.png) #101010 !important;"
-//     );
-//   }
-//   var upperBackground = document.querySelector("section.upper");
-//   if (upperBackground) {
-//     upperBackground.setAttribute("style", "background: #101010 !important;");
-//   }
-//   var containerBackground = document.querySelector(".featured-publishable");
-//   if (containerBackground) {
-//     containerBackground.setAttribute(
-//       "style",
-//       "background: #424242 !important;"
-//     );
-//   }
-//   var containerDescriptionText = document.querySelector(".description");
-//   if (containerDescriptionText) {
-//     containerDescriptionText.setAttribute("style", "color: white;");
-//   }
-//   var carouselBack = document.querySelector(".promo-carousel-revamp");
-//   if (carouselBack) {
-//     carouselBack.setAttribute("style", "background: #424242 !important;");
-//   }
-//   var titleLink = document.querySelector("a.title");
-//   if (titleLink) {
-//     titleLink.setAttribute("style", "color: #cacaca;");
-//   }
-//   document
-//     .querySelector("span.author")
-//     .setAttribute("style", "color: #e05e00;");
-//   document
-//     .querySelector("a.comment-count")
-//     .setAttribute("style", "color: #e05e00;");
-// })();
