0e582f7689d219cc3b933613c88e24b9c89aa99c
Nelson Gill <robotrogue@hotmail.com>
2019-06-19 14:04:40 -0700

    Remove even more JS

--- content.js
+++ content.js
@@ -1,153 +1,169 @@
 javascript: (function() {
-  // Modifies styling on every page regardless
-  document.querySelector("#id_term").setAttribute("style", "width: 252px;");
+                          // This bit will update their existing Social links with more up to date ones... (not dont yet)
+                          // var socialLinks = document.querySelector(
+                          //   ".header-social.header-social-top"
+                          // );
+                          // if (socialLinks) {
+                          //   socialLinks.innerHTML = '<a href="http://www.google.com/>lul</a>'
+                          // }
+                          // SVG Author Credit: <div>Icons made by <a href="https://www.flaticon.com/authors/simpleicon" title="SimpleIcon">SimpleIcon</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
-  var pageBackground = document.querySelector("#panel");
-  if (pageBackground) {
-    pageBackground.setAttribute(
-      "style",
-      "background: url(https://d2rormqr1qwzpz.cloudfront.net/static/site/img/layout/bg-grid-alt.png) #101010 !important;"
-    );
-  }
+                          var removeGooglePlus = document.querySelector(
+                            'a[href*="plus.google.com"][rel="publisher"]'
+                          );
+                          if (removeGooglePlus) {
+                            removeGooglePlus.remove();
+                          }
 
-  // This bit will update their existing Social links with more up to date ones... (not dont yet)
-  // var socialLinks = document.querySelector(
-  //   ".header-social.header-social-top"
-  // );
-  // if (socialLinks) {
-  //   socialLinks.innerHTML = '<a href="http://www.google.com/>lul</a>'
-  // }
+                          var blockAdSpam = document.querySelector(
+                            "aside.l-lastcol section.well"
+                          );
+                          if (blockAdSpam) {
+                            blockAdSpam.setAttribute(
+                              "style",
+                              "display: none"
+                            );
+                          }
 
-  var removeGooglePlus = document.querySelector(
-    'a[href*="plus.google.com"][rel="publisher"]'
-  );
-  if (removeGooglePlus) {
-    removeGooglePlus.remove();
-  }
+                          var oolaya = document.querySelector(
+                            'iframe[src*="ooyala"]'
+                          );
+                          if (oolaya) {
+                            oolaya.setAttribute(
+                              "style",
+                              "display: none"
+                            );
+                          }
 
-  var titleBar = document.querySelector("section.upper");
-  if (titleBar) {
-    titleBar.setAttribute("style", "background: #000000 !important;");
-  }
+                          var killNextVideoBox = document.querySelector(
+                            "section.more"
+                          );
+                          if (killNextVideoBox) {
+                            killNextVideoBox.remove();
+                          }
 
-  var popularNews = document.querySelector("section.popular-news");
-  if (popularNews) {
-    popularNews.setAttribute("style", "background: #424242 !important;");
-  }
+                          // Replace all the premium flag images with pure CSS
+                          var articlePremiumContainer = document.querySelectorAll(
+                            "article"
+                          );
+                          if (articlePremiumContainer) {
+                            for (
+                              var i = 0;
+                              i <
+                              articlePremiumContainer.length;
+                              i++
+                            ) {
+                              if (
+                                articlePremiumContainer[
+                                  i
+                                ].classList.contains(
+                                  "is_premium_content"
+                                )
+                              ) {
+                                articlePremiumContainer[
+                                  i
+                                ]
+                                  .querySelector(
+                                    "img.prem-flag"
+                                  )
+                                  .remove();
 
-  var adSpamBlock = document.querySelector("aside.l-lastcol section.well");
-  if (adSpamBlock) {
-    adSpamBlock.setAttribute("style", "display: none");
-  }
+                                var premiumFlag = document.createElement(
+                                  "div"
+                                );
+                                premiumFlag.innerHTML =
+                                  "PREMIUM";
+                                premiumFlag.setAttribute(
+                                  "class",
+                                  "corner-ribbon"
+                                );
+                                articlePremiumContainer[
+                                  i
+                                ].insertBefore(
+                                  premiumFlag,
+                                  articlePremiumContainer[
+                                    i
+                                  ].childNodes[0]
+                                );
+                              }
+                            }
+                          }
 
-  var recentVideos = document.querySelector("section.recent-videos");
-  if (recentVideos) {
-    recentVideos.setAttribute("style", "background: #424242 !important;");
-  }
+                          // Below applies styling to pages that contain 'feature-publishable-video' selector
+                          var hasFeatureVideo = document.querySelector(
+                            ".featured-publishable-video"
+                          );
+                          if (hasFeatureVideo) {
+                            var premVidContainer = document.querySelector(
+                              "section.is_premium_content > div.well-header-wrap"
+                            );
+                            if (premVidContainer) {
+                              var premiumTile = document.createElement(
+                                "div"
+                              );
+                              premiumTile.innerHTML =
+                                "PREMIUM";
+                              premiumTile.setAttribute(
+                                "class",
+                                "well-subheader"
+                              );
+                              premiumTile.setAttribute(
+                                "style",
+                                "color: #39d !important; border-style: solid; border-width: 1px; border-color: #39d;"
+                              );
+                              premVidContainer.appendChild(
+                                premiumTile
+                              );
+                            }
 
-  var contactBox = document.querySelector(".aside-contact");
-  if (contactBox) {
-    contactBox.setAttribute("style", "background: #424242 !important;");
-  }
+                            var premiumFlag = document.querySelector(
+                              ".prem-flag"
+                            );
+                            if (premiumFlag) {
+                              premiumFlag.remove();
+                            }
 
-  var podcastWrapper = document.querySelector(".featured-publishable-podcast");
-  if (podcastWrapper) {
-    podcastWrapper.setAttribute("style", "background: #424242 !important;");
-  }
+                            var vidPlayer = document.querySelector(
+                              "div.l-col-fluid"
+                            );
+                            if (vidPlayer) {
+                              vidPlayer.setAttribute(
+                                "style",
+                                "width: 1050px; height: 610px;"
+                              );
+                            }
 
-  var podcastWrapperText = document.querySelector("p.description");
-  if (podcastWrapperText) {
-    podcastWrapperText.setAttribute("style", "color: #cacaca;");
-  }
+                            var videoBlurb = document.querySelector(
+                              "aside.l-col-fixed"
+                            );
+                            if (videoBlurb) {
+                              videoBlurb.className =
+                                "l-col-fluid";
+                            }
 
-  var oolaya = document.querySelector('iframe[src*="ooyala"]');
-  if (oolaya) {
-    oolaya.setAttribute("style", "display: none");
-  }
+                            // var articleContainer = document.querySelector(".featured-publishable");
+                            // if (articleContainer) {
+                            //   articleContainer.setAttribute("style", "background: #424242 !important;");
+                            // }
 
-  var videoArchive = document.getElementById("video-archive");
-  if (videoArchive) {
-    videoArchive.setAttribute("style", "background: #424242 !important;");
-  }
+                            // var descriptionTitle = document.querySelector("a.title");
+                            // if (descriptionTitle) {
+                            //   descriptionTitle.setAttribute("style", "color: #cacaca;");
+                            // }
 
-  var nextVideoBox = document.querySelector('section.more');
-  if (nextVideoBox) {
-    nextVideoBox.remove();
-  }
+                            // var author = document.querySelector("span.author");
+                            // if (author) {
+                            //   author.setAttribute("style", "color: #e05e00;");
+                            // }
 
-  // Replace all the premium flag images with pure CSS
-  var articlePremiumContainer = document.querySelectorAll("article");
-  if (articlePremiumContainer) {
-    for (var i = 0; i < articlePremiumContainer.length; i++) {
-      if (articlePremiumContainer[i].classList.contains("is_premium_content")) {
-        articlePremiumContainer[i].querySelector("img.prem-flag").remove();
+                            // var descriptionText = document.querySelector(".description");
+                            // if (descriptionText) {
+                            //   descriptionText.setAttribute("style", "color: white;");
+                            // }
 
-        var premiumFlag = document.createElement("div");
-        premiumFlag.innerHTML = "PREMIUM";
-        premiumFlag.setAttribute("class", "corner-ribbon");
-        articlePremiumContainer[i].insertBefore(
-          premiumFlag,
-          articlePremiumContainer[i].childNodes[0]
-        );
-      }
-    }
-  }
-
-  // Below applies styling to pages that contain 'feature-publishable-video' selector
-  var hasFeatureVideo = document.querySelector(".featured-publishable-video");
-  if (hasFeatureVideo) {
-    var premVidContainer = document.querySelector(
-      "section.is_premium_content > div.well-header-wrap"
-    );
-    if (premVidContainer) {
-      var premiumTile = document.createElement("div");
-      premiumTile.innerHTML = "PREMIUM";
-      premiumTile.setAttribute("class", "well-subheader");
-      premiumTile.setAttribute(
-        "style",
-        "color: #39d !important; border-style: solid; border-width: 1px; border-color: #39d;"
-      );
-      premVidContainer.appendChild(premiumTile);
-    }
-
-    var premiumFlag = document.querySelector(".prem-flag");
-    if (premiumFlag) {
-      premiumFlag.remove();
-    }
-
-    var vidPlayer = document.querySelector("div.l-col-fluid");
-    if (vidPlayer) {
-      vidPlayer.setAttribute("style", "width: 1050px; height: 610px;");
-    }
-
-    var videoBlurb = document.querySelector("aside.l-col-fixed");
-    if (videoBlurb) {
-      videoBlurb.className = "l-col-fluid";
-    }
-
-    // var articleContainer = document.querySelector(".featured-publishable");
-    // if (articleContainer) {
-    //   articleContainer.setAttribute("style", "background: #424242 !important;");
-    // }
-
-    // var descriptionTitle = document.querySelector("a.title");
-    // if (descriptionTitle) {
-    //   descriptionTitle.setAttribute("style", "color: #cacaca;");
-    // }
-
-    // var author = document.querySelector("span.author");
-    // if (author) {
-    //   author.setAttribute("style", "color: #e05e00;");
-    // }
-
-    // var descriptionText = document.querySelector(".description");
-    // if (descriptionText) {
-    //   descriptionText.setAttribute("style", "color: white;");
-    // }
-
-    // var promoCarousel = document.querySelector(".promo-carousel-revamp");
-    // if (promoCarousel) {
-    //   promoCarousel.setAttribute("style", "background: #424242 !important;");
-    // }
-  }
-})();
+                            // var promoCarousel = document.querySelector(".promo-carousel-revamp");
+                            // if (promoCarousel) {
+                            //   promoCarousel.setAttribute("style", "background: #424242 !important;");
+                            // }
+                          }
+                        })();
